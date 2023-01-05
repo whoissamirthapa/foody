cc5057681174d6ca9a0cef612d3a1ea4404c5777
Samir Thapa <thapasamirm577@gmail.com>
2023-01-05 20:36:27 +0545

    fhalkdf-contact-front-fahdlf

--- controller/company_profile.controller.ts
+++ controller/company_profile.controller.ts
@@ -2,20 +2,21 @@ import { Request, Response } from "express";
 import ContactUs from "../model/company_profile.model.js";
 
 export const getContactUs = async (req: Request, res: Response) => {
-    const { name, address_location, email, mobile_no, subject, message } =
-        req.body;
-    try {
-        const contactUs = new ContactUs({
-            name,
-            address_location,
-            email,
-            mobile_no,
-            subject,
-            message,
-        });
-        await contactUs.save();
-        res.status(200).json({ message: "Message sent successfully" });
-    } catch (error) {
-        res.status(500).json({ error: "Message send error!" });
-    }
+  const { name, address_location, email, mobile_no, subject, message } =
+    req.body;
+  console.log(req.body);
+  try {
+    const contactUs = new ContactUs({
+      name,
+      address_location,
+      email,
+      mobile_no,
+      subject,
+      message,
+    });
+    await contactUs.save();
+    res.status(200).json({ message: "Message sent successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Message send error!" });
+  }
 };
